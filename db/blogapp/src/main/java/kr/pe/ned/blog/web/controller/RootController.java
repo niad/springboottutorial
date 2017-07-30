c7163028c3dcf92985c5a47d5ca19c42432af0ba
ned <niad1477@gmail.com>
2017-07-30 11:15:28 +0900

    jpa 사용 예제 추가

---
+++ db/blogapp/src/main/java/kr/pe/ned/blog/web/controller/RootController.java
@@ -0,0 +1,19 @@
+package kr.pe.ned.blog.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping
+public class RootController {
+
+
+    @RequestMapping(value="/")
+    public ModelAndView list() {
+
+        return new ModelAndView("index");
+    }
+
+}
+
